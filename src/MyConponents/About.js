60fa598428dfc8b5b7c6a7438ce7bfcb019e66f4
Sushant Yerawar <sushantdyerawar2002@gmail.com>
2022-01-15 01:42:33 +0530

    deploy my react app to github pages

---
+++ src/MyConponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about Component
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum repellat excepturi quos iure magnam ducimus, fuga mollitia, soluta aperiam recusandae nisi saepe nam adipisci culpa neque veritatis dolorum nesciunt sint!</p>
+        </div>
+    )
+}
---
+++ src/MyConponents/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./footer.css"
+export const Footer = () => {
+    // let footerStyle={
+    //     position:"relative",
+    //     top:"10vh",
+    //     width:" 100%",
+    //     border: "2px solid pink"
+    // }
+    return (
+        // <footer className= "bg-dark text-light py-3" style={footerStyle}>
+        <footer className= "bg-dark text-light py-3" >
+            <p className="text-center">
+           Copyright &copy; MyTodoList.com 
+           </p>
+        </footer>
+    )
+}
